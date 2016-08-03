a480b044191e36b60f0af995414dfba71d6b7958
phong le <phong.ld@3si.vn>
2016-08-03 11:45:56 +0700

    Using Quartz for scheduling.

---
+++ Deiofiber/Common/ContractUpdateJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Deiofiber.Common
+{
+    public class ContractUpdateJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            CommonList.AutoExtendContract();
+        }
+    }
+}
---
+++ Deiofiber/Common/JobScheduler.cs
@@ -0,0 +1,27 @@
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace Deiofiber.Common
+{
+    public class JobScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<ContractUpdateJob>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     s.WithIntervalInHours(24)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 1))
+                  )
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
